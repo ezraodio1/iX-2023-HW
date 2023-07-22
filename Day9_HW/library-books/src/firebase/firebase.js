8e656ee8b411a247450706cbd5d6d0638b3831a7
Ezra Odio <ezra.odio@duke.edu>
2023-07-22 21:56:28 +0100

    finished day 9 hw

---
+++ Day9_HW/library-books/src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBygNqRJ3bJspWBJf0YtPbw0xsETwz_fhM",
+  authDomain: "library-books-81a60.firebaseapp.com",
+  projectId: "library-books-81a60",
+  storageBucket: "library-books-81a60.appspot.com",
+  messagingSenderId: "259206198283",
+  appId: "1:259206198283:web:fb6413dacfa17fcfa9830e"
+};
+
+// Initialize Firebase and Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
