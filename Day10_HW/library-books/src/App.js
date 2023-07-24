19fca4c00f66d851a2cfdbba66bfbe30dc2265f0
Ezra Odio <ezra.odio@duke.edu>
2023-07-24 13:31:10 +0100

    completed day 10

---
+++ Day10_HW/library-books/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import {auth} from './firebase/firebase';
+
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import BookPage from './components/book/BookPage';
+import LoginPage from './components/auth/LoginPage';
+import RegisterPage from './components/auth/RegisterPage';
+import Navbar from './components/common/Navbar';
+
+function App() {
+
+    const[user, setUser] = useState(null);
+
+    useEffect(() => {
+      onAuthStateChanged(auth, (user) => {
+        setUser();
+      });
+    }, []);
+  
+  return (
+    <BrowserRouter>
+      <Navbar user={user}/>
+      <Routes>
+        <Route path ='/' element={<BookPage />}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/register' element={<RegisterPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
